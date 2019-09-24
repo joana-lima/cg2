c623d178618a1412ff3ce53a8a3189645aba9d09
Louis Ian <louisiangt@gmail.com>
2019-09-23 21:12:17 -0300

    Projeto Visual Studio

---
+++ Computação Gráfica 1/Computação Gráfica 1/RenderAPI.h
@@ -0,0 +1,158 @@
+#pragma once
+
+//
+//	This is to make a bridge btw the code and the grapgic API
+//	In this file we are using the OpenGL API, but can be easily changed without changing the main code
+//
+
+
+#include "GL/glew.h"
+#include "GL/glut.h"
+#include "GL/freeglut.h"
+
+class Color {
+    public:
+    int r;
+    int g;
+    int b;
+};
+
+namespace RenderAPI{
+
+	typedef GLuint VertexBuffer;
+
+
+	inline void StartRenderAPI(int argc, char **argv, int width, int height)
+	{
+		// init glut:
+		glutInit(&argc, argv);
+		
+		// specify the display mode to be RGB and mutiple buffering:
+		glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_MULTISAMPLE);
+		// specify the initial window position:
+		glutInitWindowPosition(500, 100);
+		// specify the initial window size:
+		glutInitWindowSize(width, height);
+		// create the window and set title:
+		glutCreateWindow("CG");
+		
+		//disable some opengl functions that will not be used
+		glDisable(GL_DEPTH_TEST);
+		glDisable(GL_LIGHTING);
+
+		//set the opengl background color
+		glClearColor(0.0, 1.0, 0.0, 0.0);
+		glMatrixMode(GL_PROJECTION);
+		//tell what will be the type and size of the view
+		//in this case we are using a 2d ortho with size width by heigth because we want to every pixel be a point
+		gluOrtho2D(0.0, width, 0.0, height);
+
+		//init glew to handle with the buffers
+		glewInit();
+		if (!glewIsSupported("GL_VERSION_2_0 ")) {
+			exit(0);
+		}
+	}
+
+	inline void setView(int width, int height) {
+		glViewport(0, 0, width, height);
+		
+		glMatrixMode(GL_PROJECTION);
+		glPushMatrix();
+		glLoadIdentity();
+		gluOrtho2D(0.0, width, 0.0, height);
+	}
+
+
+	//clear the buffer 
+	inline void BufferClear() {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+
+	//start the API render lood
+	inline void RenderLoop()
+	{
+		glutMainLoop();
+	}
+
+	//Create a vertex buffer
+	inline void CreateVBO(VertexBuffer *_vbo, int width, int height)
+	{
+		if (!_vbo)return;
+		//Create vertex buffer object
+		glGenBuffers(1, _vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, *_vbo);
+
+		//Initialize VBO
+		//set the size of the buffer
+		//look that we multiply by 5 because we want 2 floats to be the pixel position and 3 to be the pixel color
+		unsigned int size = width * height * sizeof(float) * 5;
+		glBufferData(GL_ARRAY_BUFFER, size, 0, GL_DYNAMIC_DRAW);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+
+	inline void DeleteVBO(VertexBuffer* _vbo)
+	{
+		glDeleteBuffers(1, _vbo);
+	}
+
+	//bind the buffer with a cpu vertex buffer that can be changed by the user
+	inline void BufferBind(const VertexBuffer &vbo){
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	}
+
+	//TODO: Use your color struct here
+	//Map the buffer data to the graphics card 
+	inline void MapBuffer(Color *color_buffer, int width, int height){
+		//get the graphics card actual buffer
+		float *buffer = (float*) glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+		if (!buffer)return;
+		Color *c_buff = color_buffer;
+		
+		
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++, c_buff++) {
+				//set the pix position 
+				buffer[(y*width + x) * 5 + 0] = (float) x;
+				buffer[(y*width + x) * 5 + 1] = (float) y;
+				//set the pix rgb values
+				buffer[(y*width + x) * 5 + 2] = c_buff->r;
+				buffer[(y*width + x) * 5 + 3] = c_buff->g;
+				buffer[(y*width + x) * 5 + 4] = c_buff->b;
+			}
+		}
+		//Unmap the buffer, this tell the GC (graphics card) that we have finished our cahnges
+		glUnmapBuffer(GL_ARRAY_BUFFER);
+
+		//here we tell the GC how our data is organized
+		//here we tell taht we are using 2 floats for position, the total pixel size in floats is 20 and the position is in the posi 0 (the start)
+		glVertexPointer(2, GL_FLOAT, 20, 0);
+		//here we tell that we are using 3 floats for color, the total pixel size in floats is 20 and the color is in the posi 8
+		//(because the position occupied the 8 first bytes)
+		glColorPointer(3, GL_FLOAT, 20, (GLvoid*)8);
+
+		//tell the CG to draw our data
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glEnableClientState(GL_COLOR_ARRAY);
+		//will draw our pixels where every one is a point, he will start at posi 0 of our buffer and will draw width * height pixels
+		glDrawArrays(GL_POINTS, 0, width * height);
+		glDisableClientState(GL_VERTEX_ARRAY);
+	}
+
+
+	inline void SwapBuffers(){
+		glutSwapBuffers();
+	}
+
+	inline void ReDisplay() {
+		glutPostRedisplay();
+	}
+	//Event handlers
+	inline void KeyboardFunc(void(*callback)(unsigned char, int, int))		{ glutKeyboardFunc(callback); }
+	inline void ReshapeFunc(void(*callback)(int, int))						{ glutReshapeFunc(callback); }
+	inline void DisplayFunc(void(*callback)(void))							{ glutDisplayFunc(callback); }
+	inline void MouseFunc(void(*callback)(int, int, int, int))				{ glutMouseFunc(callback); }
+	inline void MotionFunc(void(*callback)(int, int))						{ glutMotionFunc(callback); }
+	
+}
---
+++ Computação Gráfica 1/Computação Gráfica 1/teste.cpp
@@ -0,0 +1,228 @@
+#include "Objetos.cpp"
+
+using namespace std;
+
+int main() {
+
+    cout << "\n-----------Teste de Matriz------------" << endl;
+    vector<vector<double>> a = {{2, 3},
+                                {1, 0},
+                                {4, 5}};
+
+    vector<vector<double>> b = {{3, 1},
+                                {2, 4}};
+
+    Matriz* A = new Matriz(a);
+    Matriz* B = new Matriz(b);
+
+    A->print();
+    cout << endl;
+
+    B->print();
+    cout << endl;
+
+    Matriz* C = A->produto(B);
+
+    C->print(); //deve printar:
+                // 12 14 
+                // 3  1
+                // 22 24
+    cout << endl;
+
+    cout << "\n-----------Teste de Equação do 2° grau------------" << endl;
+
+    vector<double> raizes1 = equacaoSegundoGrau(1, 2, 1);
+    cout << "\nRaízes de x² + 2x + 1:" << endl;
+    cout << raizes1.size() << " raízes\n";
+    for(int i = 0; i < raizes1.size(); i++){
+        cout << raizes1[i] << endl;
+    }
+
+    vector<double> raizes2 = equacaoSegundoGrau(2, 2, -1);
+    cout << "\nRaízes de 2x² + 2x - 1:" << endl;
+    cout << raizes2.size() << " raízes\n";
+    for(int i = 0; i < raizes2.size(); i++){
+        cout << raizes2[i] << endl;
+    }
+    
+    vector<double> raizes3 = equacaoSegundoGrau(1, 2, 0);
+    cout << "\nRaízes de x² + 2x:" << endl;
+    cout << raizes3.size() << " raízes\n";
+    for(int i = 0; i < raizes3.size(); i++){
+        cout << raizes3[i] << endl;
+    }
+
+    double resultado;                    //Escalar qualquer
+    Ponto *ponto1 = new Ponto(1,1,1);
+    Ponto *ponto2;
+    Ponto *ponto3 = new Ponto(3,-2, 0);
+    Vetor *vetor1 = new Vetor(1,1,1);   //Vetores para testes
+    Vetor *vetor2 = new Vetor(2,2,2);
+    Vetor *vetor3;
+    Vetor *vetor4;
+    Vetor *vetor5 = new Vetor(1,2,0);
+    Vetor *vetor6 = new Vetor(3,-2,0);
+    Vetor *vetor7 = new Vetor(0,0,1);
+
+    // **********INICIO TESTE DA CLASSE VETOR***************
+
+    cout << "\n-----------Teste print------------" << endl;
+    vetor1->print();
+    vetor2->print();
+
+    cout << "\n------------Teste Produto Escalar------------" << endl;
+    resultado = vetor1->produtoEscalar(*vetor2);
+    cout << resultado << endl;
+
+    cout << "\n------------Teste Multiplicação por Escalar-----------" << endl;
+    vetor3 = vetor2->multEscalar(4);
+    vetor3->print();
+    vetor3 = vetor2->multEscalar(0);
+    vetor3->print();
+    
+    cout << "\n--------------Teste Cálculo da Norma------------" << endl;
+    resultado = vetor1->calcularNorma();
+    cout << "vetor1 - Norma: " << resultado << endl;
+    cout << "vetor2 - Norma: " << resultado << endl;
+
+
+    cout << "\n--------------Teste Normalizar----------" << endl;
+    vetor4 = new Vetor(4,2,8);
+    vetor4 = vetor4->normalizar();
+    vetor4->print();
+
+    cout << "\n---------------Teste do operador + --------------- " << endl;
+    *vetor3 = *vetor1 + *vetor2;
+    vetor3->print();
+
+    cout << "\n---------------Teste do operador - ----------------" << endl;
+    *vetor3 = *vetor1 - *vetor2;
+    vetor3->print();
+
+    cout << "\n---------------Teste do operador * (produto escalar)---------" << endl;
+    resultado = *vetor1 * *vetor2;
+    cout << "produto escalar: " << resultado << endl;
+
+    cout << "\n-----------Teste Produto Vetorial------------------" << endl;
+    vetor3 = vetor5->produtoVetorial(*vetor6);
+    vetor3->print();
+
+    //****************FIM TESTES DA CLASSE VETOR******************
+    
+    //****************TESTES DA CLASSE RETA***********************
+    cout << "---------Teste de instanciação da reta e print------------------" << endl;
+    Reta *reta1 = new Reta(*ponto1, *vetor7);
+    reta1->print();
+
+    cout << "---------Teste do ponto atingido-------" << endl;
+    ponto2 = reta1->pontoAtingido(3);
+    ponto2->print();
+
+    //***************FIM DOS TESTES DA CLASSE RETA*******************
+
+    //***************INICIO TESTES CLASSE PLANO**********************
+    cout << "----------Teste do pertencePlano----------------" << endl;
+    Plano *meuplano = new Plano(Ponto(0, 0, 0), Vetor(0, 0, 1));
+    Ponto *ponto4 = new Ponto(1, 1, 0);
+    bool teste = meuplano->pertencePlano(*ponto4);
+    cout << teste << endl;
+
+    cout << "---------Teste da interseção raio-plano----" << endl; //Por fazer.
+
+    Vetor *meuvetor = new Vetor(0,0,-1);
+    Reta *minhareta = new Reta(Ponto(0, 0, 1),*meuvetor);
+    vector<Ponto> intersecao = meuplano->intRaio(*minhareta);
+    vector<Ponto>::iterator i;
+    for(i=intersecao.begin(); i!=intersecao.end(); i++) {
+        i->print();
+    }
+
+    cout << "---------Teste da interseçao do cilindro------" << endl;
+
+    //Criando o cilindro
+    Ponto *base = new Ponto(2,0,0);
+    Vetor *normal = new Vetor(0,1,0);
+    
+    //Reta que colidirá com o cilindro
+    Ponto *pontoInicio = new Ponto(-4,1,0);
+    Vetor *direcaoReta = new Vetor(1,0,0);
+    Reta *reta2 = new Reta(*pontoInicio,*direcaoReta);
+    Cilindro *cilindro1 = new Cilindro(*base, *normal, 2, 4);
+    
+
+    vector<Ponto> cilindroIntersecao = cilindro1->intRaio(*reta2);
+    vector<Ponto>::iterator k;
+    for(k=cilindroIntersecao.begin(); k!=cilindroIntersecao.end(); k++) {
+        k->print();
+    }
+
+ cout << "---------Teste de Objeto-------------------" << endl;
+    Objeto *obj1 = new Objeto();
+    Ponto *v1 = new Ponto(0, 0, 0);
+    Ponto *v2 = new Ponto(1, 0, 0);
+    Ponto *v3 = new Ponto(0, 1, 0);
+    Ponto *v4 = new Ponto(0, 0, 1);
+
+    obj1->setVisibilidade(false);
+
+    obj1->adicionarVertice(*v1);
+    obj1->adicionarVertice(*v2);
+    obj1->adicionarVertice(*v3);
+    obj1->adicionarVertice(*v4);
+
+    obj1->adicionarAresta(*v1, *v2);
+    obj1->adicionarAresta(*v1, *v3);
+    obj1->adicionarAresta(*v1, *v4);
+    obj1->adicionarAresta(*v2, *v3);
+    obj1->adicionarAresta(*v3, *v4);
+    obj1->adicionarAresta(*v4, *v2);
+
+    obj1->adicionarFace(*v1, *v3, *v2);
+    obj1->adicionarFace(*v1, *v3, *v4);
+    obj1->adicionarFace(*v1, *v4, *v2);
+    obj1->adicionarFace(*v2, *v3, *v4);
+
+    obj1->print();
+
+    cout << "---------Teste de Triangulo-------------------" << endl;
+    Triangulo *triangulo = new Triangulo(*v1, *v2, *v3);
+    Reta *retaTrianguloMiss = new Reta(Ponto(0.0, -0.2, -1.0), Vetor(0.0, 0.0, -1.0));
+    vector<Ponto> intTrianguloMiss = triangulo->intRaio(*retaTrianguloMiss);
+    // triangulo->print();
+
+    for(int i = 0; i < intTrianguloMiss.size(); i++){
+        intTrianguloMiss[i].print();
+    }
+
+    Reta *retaTrianguloHit = new Reta(Ponto(0.1, 0.2, -1.0), Vetor(0.0, 0.0, -1.0));
+    vector<Ponto> intTrianguloHit = triangulo->intRaio(*retaTrianguloHit);
+    // triangulo->print();
+
+    for(int i = 0; i < intTrianguloHit.size(); i++){
+        intTrianguloHit[i].print();
+    }
+
+    cout << "\n---------Teste de Cubo-------------------" << endl;
+    Cubo *cubo = new Cubo(Ponto(0.0, 0.0, 0.0), 2.0, Vetor(0.0, 1.0, 0.0));
+    Reta *retaCuboMiss = new Reta(Ponto(2.0, 2.0, -1.0), Vetor(0.0, 0.0, -1.0));
+    Reta *retaCuboHit = new Reta(Ponto(0.0, 0.0, -1.0), Vetor(0.0, 0.0, -1.0));
+
+    
+    cout << "CUBO" << endl;
+    cubo->print();
+    cout << endl;
+
+    vector<Ponto> intCuboHit = cubo->intRaio(*retaCuboHit);
+    vector<Ponto> intCuboMiss = cubo->intRaio(*retaCuboMiss);
+
+    cout << "\ninicio de teste que deve errar: ";
+    for(int i = 0; i < intCuboMiss.size(); i++){
+        intCuboMiss[i].print();
+    }
+
+    cout << "\ninicio de teste que deve acertar:\n";
+    for(int i = 0; i < intCuboHit.size(); i++){
+        intCuboHit[i].print();
+    }
+
+}
